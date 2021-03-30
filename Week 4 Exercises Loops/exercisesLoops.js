c717d3f42b658b9776ccb8fb7953f4a094fb2735
batacira <batacira@gmail.com>
2021-03-30 23:52:46 +0200

    Arrays and Loops exercises

--- Week 4 Exercises Loops/exercisesLoops.js
+++ Week 4 Exercises Loops/exercisesLoops.js
@@ -46,6 +46,25 @@ for(var i = 0; i < x.length; i++) {
     console.log(x[i]);
 }
 
+//5. Write a program that prints the elements of the following array.
+// var a = [
+
+//     [1, 2, 1, 24],
+//     [8, 11, 9, 4],
+//     [7, 0, 7, 27]
+// ];
+
+var a = [[1, 2, 1, 24],[8, 11, 9, 4],[7, 0, 7, 27]];
+var result = '';
+
+for(var i = 0; i < a.length; i++) {
+    for(var j = 0; j < a[i].length; j++) {
+        result += a[i][j] + "\t";
+    }
+    result += "\n";
+}
+
+console.log(result);
 
 //6. Write a program that outputs the sum of squares of the first 20 numbers.
 
@@ -55,3 +74,83 @@ for(var i = 0; i <= 20; i++) {
 } 
 console.log(suma);
 
+// 7. Write a program that computes average marks of the following students.Then use this
+// average to determine the corresponding grade.
+
+// David 80
+// Marko 77
+// Dany 88
+// John 95
+// Thomas 68
+// The grades are computed as follows :
+
+// < 60 % -  F
+// < 70 % -  D
+// < 80 % -  C
+// < 90 % -  B
+// < 100 % - A
+
+var niz = [['David', 80], ['Marko', 77], ['Dany', 88], ['John', 95], ['Thomas', 68]];
+var avg = 0;
+
+for (var i = 0; i < niz.length; i++) {
+    avg += niz[i][1]; // niz 0 pa ulazi u 1, pa niz 1 pa opet u 1, pa 2 pa opet u njegov 1 itd..
+    var average = avg / niz.length;
+}
+
+if (average < 60 && average >= 50) {
+    console.log('F');
+}
+else if (average < 70) {
+    console.log('D');
+}
+else if (average < 80) {
+    console.log('C');
+}
+else if (average < 90) {
+    console.log('B');
+}
+else if (average < 100) {
+    console.log('A');
+}
+else {
+    console.log('Not between 50 and 100');
+}
+
+// 8. Write a program that uses console.log to print all the numbers from 1 to 100, with two
+// exceptions.For numbers divisible by 3, print Fizz  instead of the number, and for numbers
+// divisible by 5(and not 3), Buzz instead.When you have that working, modify your
+// program to print FizzBuzz, for numbers that are divisible by both 3 and 5(and still print
+//     Fizz  or Buzz  for numbers divisible by only one of those).
+
+
+for (var i = 0; i <= 100; i++) {
+    if (i % 3 === 0) {
+        console.log('Fizz');
+    }
+    else {
+        if (i % 5 === 0 && i % 3 !== 0) {
+            console.log('Buzz');
+        }
+        else {
+            console.log(i);
+        }
+    }
+}
+
+
+
+for (var i = 0; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+    }
+    else if (i % 3 === 0) {
+        console.log('Fizz')
+    }
+    else if (i % 5 === 0) {
+        console.log('Buzz')
+    }
+    else {
+        console.log(i);
+    }
+}
---
+++ Week 6 Loops/tasks.js
@@ -0,0 +1,200 @@
+//1. Write a program that calculates the maximum of two given numbers.
+function max(a, b) {
+    var s = 'The numbers are equal';
+    if( a > b) {
+        return a;
+    }
+    else if(b > a) {
+        return b;
+    }
+    else {
+        return s;
+    }
+}
+
+console.log (max(20, 20));
+
+//2. Write a program that checks if a given number is odd.
+function isOdd(n) {
+    if(n % 2 === 1) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+console.log(isOdd(6));
+
+//3. Write a program that checks if a given number is a three digit long number.
+function isThreeDigit(a) {
+    if(a >= 100 && a < 1000 || a > -1000 && a <= -100 ) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log(isThreeDigit(10));
+
+//4. Write a program that calculates an arithmetic mean of four numbers.
+
+function arMean(a, b, c, d) {
+    var sum = a + b + c + d;
+    var arMean = sum / 4;
+    return arMean;
+}
+
+console.log(arMean(2,24,12,10));
+
+// 5.  Write a program that draws a square of a given size.For example, if the size of square
+// is 5 the program should draw:
+// *****
+// * *
+// * *
+// * *
+// *****
+
+
+function drawSquare(n) {
+    var square = '';
+    for (var i = 0; i < n; i++) {
+        for (var j = 0; j < n; j++) {
+            if (i === 0 || i === n - 1) {
+                square += '* ';
+            } else if (j === 0 || j === n - 1) {
+                square += '* ';
+            } else {
+                square += '  ';
+            }
+        }
+        square += '\n';
+    }
+    return square;
+}
+
+var square1 = drawSquare(3);
+
+console.log(square1);
+
+
+// 6. Write a program that draws a horizontal chart representing three given values.For
+// example, if values are 5, 3, and 7, the program should draw:
+// * * * * *
+// * * *
+// * * * * * * *
+
+function horizontalChart2(num1, num2, num3) {
+    var star = '';
+    for (var i = 0; i < num1; i++) {
+        star += '*';
+    }
+    star += '\n';
+    for (var i = 0; i < num2; i++) {
+        star += '*';
+    }
+    star += '\n';
+    for (var i = 0; i < num3; i++) {
+        star += '*';
+    }
+    return star;
+}
+
+console.log(horizontalChart2(3, 13, 3));
+
+
+function horizontalChart2() {
+    var star = '*';
+
+    
+}
+
+console.log(horizontalChart2(3, 13, 3));
+
+
+
+//7. Write a program that calculates a number of digits of a given number.
+
+function numDigits(num) {
+    var count = 0;
+    var absNum = num;
+
+    if(num < 0) {
+        num *= -1;
+    }
+
+    for(var i = absNum; i >= 1; i/= 10) {
+        count++;
+    }
+    return count;
+}
+
+console.log((numDigits(100)));
+
+//8. Write a program that calculates a number of appearances of a given number in a given
+// array.
+//     Inputs: a = [2, 4, 7, 8, 7, 7, 1], e = 7
+// Result: 3
+
+var a = [2, 4, 7, 8, 7, 7, 1];
+var e = 7;
+
+function givNum(a, e) {
+    var count = 0;
+    for(var i = 0; i < a.length; i++) {
+        if(a[i] === e) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(givNum(a, e));
+
+//9. Write a program that calculates the sum of odd elements of a given array.
+var a = [2, 4, 7, 8, 7, 7, 1];
+
+function sumOdd(a) {
+    var sum = 0;
+    for(var i = 0; i < a.length; i++) {
+        if(a[i] % 2 === 1) {
+            sum += a[i];
+        }
+    }
+    return sum;
+}
+
+console.log(sumOdd(a));
+
+//10. Write a program that calculates the number of appearances of a letter a in a given string.
+//Modify the program so it calculates the number of both letters a and A.
+
+function letterA(string) {
+    count = 0;
+    for(var i = 0; i < string.length; i++) {
+        var lett = string[i];
+        if(lett === 'a' || lett === 'A') {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(letterA('Banana'));
+
+//11. Write a program that concatenates a given string given number of times. For example, if
+//“abc” and 4 are given values, the program prints out abcabcabcabc.
+
+
+function conc(string, num) {
+    var p = num;
+    var help = '';
+    for (var i = 0; i < p; i++) {
+        help += string;
+    }
+    return help;
+}
+
+console.log(conc('Milos', 4));
+
+
